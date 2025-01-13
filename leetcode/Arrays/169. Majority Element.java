3f1cc242453a4652ff1b475325d21d20dba3b37d
ashwinn-si <ashwin23cse@gmail.com>
2025-01-13 22:33:08 +0530

    Done ..!

---
+++ leetcode/Arrays/169. Majority Element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int currElement = 0;
+        int len = nums.length;
+        for(int i = 0 ; i < len ; i++){
+            if(count == 0){
+                currElement = nums[i];
+                count = 1;
+            }else if( nums[i] != currElement){
+                count--;
+            }else{
+                count++;
+            }
+        }
+    
+        return currElement;
+    }
+}

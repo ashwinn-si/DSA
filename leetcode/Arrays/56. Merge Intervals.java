516e97051e6a861cdaad4949de25d9724ee04518
ashwinn-si <ashwin23cse@gmail.com>
2025-07-03 21:28:10 +0530

    DONE...!

--- leetcode/Arrays/56. Merge Intervals.java
+++ leetcode/Arrays/56. Merge Intervals.java
@@ -1,28 +1,31 @@
 class Solution {
-    public int[][] merge(int[][] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        int len = nums.length;
-        int pointer = 0;
-        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 0; i < len; i++) {
-            if (i == 0) {
-                result.add(new ArrayList<>(Arrays.asList(nums[0][0], nums[0][1])));
-                continue;
-            }
-            if (result.get(pointer).get(1) >= nums[i][0]) {
-                result.get(pointer).set(1, Math.max(nums[i][1], result.get(pointer).get(1)));
-                result.get(pointer).set(0, Math.min(nums[i][0], result.get(pointer).get(0)));
-            } else {
-                result.add(new ArrayList<>(Arrays.asList(nums[i][0], nums[i][1])));
-                pointer++;
-            }
+  public int[][] merge(int[][] intervals) {
+    List<List<Integer>> result = new ArrayList<>();
+    int N = intervals.length;
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    int start = -1, end = -1;
+    for (int i = 0; i < N; i++) {
+      int currIndexStartValue = intervals[i][0];
+      int currIndexEndValue = intervals[i][1];
+      if (start == -1 && end == -1) {
+        start = currIndexStartValue;
+        end = currIndexEndValue;
+      } else {
+        if (currIndexStartValue >= start && currIndexStartValue <= end) {
+          end = Math.max(end, currIndexEndValue);
+        } else {
+          result.add(Arrays.asList(start, end));
+          start = currIndexStartValue;
+          end = currIndexEndValue;
         }
-        len = result.size();
-        int returnResult[][] = new int[len][2];
-        for (int i = 0; i < len; i++) {
-            returnResult[i][0] = result.get(i).get(0);
-            returnResult[i][1] = result.get(i).get(1);
-        }
-        return returnResult;
+      }
     }
+    result.add(Arrays.asList(start, end));
+    int returnResult[][] = new int[result.size()][2];
+    for (int i = 0; i < result.size(); i++) {
+      returnResult[i][0] = result.get(i).get(0);
+      returnResult[i][1] = result.get(i).get(1);
+    }
+    return returnResult;
+  }
 }

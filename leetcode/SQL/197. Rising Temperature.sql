ccdf1cafa764d328a174acef49bc92e608e063ed
ashwinn-si <ashwin23cse@gmail.com>
2025-09-27 17:42:38 +0530

    DONE...!

---
+++ leetcode/SQL/197. Rising Temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select today.id from weather yesterday cross join weather today where DATEDIFF(today.recordDate, yesterday.recordDate) = 1 and today.temperature > yesterday.temperature;

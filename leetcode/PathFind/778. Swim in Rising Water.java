c4d2d4103ff543ac9c4c3b56a8f24cf9b342790b
ashwinn-si <ashwin23cse@gmail.com>
2025-10-07 04:51:25 +0530

    DONE...!

--- leetcode/PathFind/778. Swim in Rising Water.java
+++ leetcode/PathFind/778. Swim in Rising Water.java
@@ -1,53 +1,45 @@
 class Solution {
-  int R;
-  int C;
-  PriorityQueue<Coor> pq = new PriorityQueue<>();
-  int offsets[][] = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-  int visited[][];
-  int maxValue = 0;
+  static int offsets[][] = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+  static int R, C;
 
-  boolean isValid(int currR, int currC) {
+  static boolean isValid(int currR, int currC) {
     return currR >= 0 && currR < R && currC >= 0 && currC < C;
-  }
-
-  void populate(Coor currCoor, int matrix[][]) {
-    for (int i = 0; i < 4; i++) {
-      int newR = currCoor.row + offsets[i][0];
-      int newC = currCoor.col + offsets[i][1];
-      if (isValid(newR, newC) && visited[newR][newC] == 0) {
-        visited[newR][newC]++;
-        pq.offer(new Coor(newR, newC, matrix[newR][newC]));
-      }
-    }
   }
 
   public int swimInWater(int[][] matrix) {
     R = matrix.length;
     C = matrix[0].length;
-    visited = new int[R][C];
+
+    PriorityQueue<Coor> pq = new PriorityQueue<>((a, b) -> a.minHeight - b.minHeight);
     pq.offer(new Coor(0, 0, matrix[0][0]));
-    while (true) {
+    matrix[0][0] = Integer.MAX_VALUE;
+
+    while (!pq.isEmpty()) {
       Coor currCoor = pq.poll();
-      maxValue = Math.max(currCoor.val, maxValue);
       if (currCoor.row == R - 1 && currCoor.col == C - 1) {
-        return maxValue;
+        return currCoor.minHeight;
       }
-      populate(currCoor, matrix);
+      for (int index = 0; index < 4; index++) {
+        int newR = currCoor.row + offsets[index][0];
+        int newC = currCoor.col + offsets[index][1];
+        if (isValid(newR, newC) && matrix[newR][newC] != Integer.MAX_VALUE) {
+          pq.offer(new Coor(newR, newC, Math.max(matrix[newR][newC], currCoor.minHeight)));
+          matrix[newR][newC] = Integer.MAX_VALUE;
+        }
+      }
     }
+
+    return -1;
   }
 }
 
-class Coor implements Comparable<Coor> {
-  int row, col, val;
+class Coor {
+  int minHeight;
+  int row, col;
 
-  Coor(int row, int col, int val) {
+  Coor(int row, int col, int minHeight) {
     this.row = row;
     this.col = col;
-    this.val = val;
-  }
-
-  @Override
-  public int compareTo(Coor other) {
-    return this.val - other.val;
+    this.minHeight = minHeight;
   }
 }

ff7f943a5ac106603f5c5dc597f4e3b07f141189
ashwinn-si <ashwin23cse@gmail.com>
2025-10-27 23:56:18 +0530

    MAC COMMIT FIRST

---
+++ leetcode/SQL/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,9 @@
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) as num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM
+    Activities
+GROUP BY
+    sell_date
+;

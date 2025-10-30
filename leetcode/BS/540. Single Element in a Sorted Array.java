37bb15b6f40309e53fe8ef44244591e50555d0cb
ashwinn-si <ashwin23cse@gmail.com>
2025-10-30 10:37:09 +0530

    DONEE..!

--- leetcode/BS/540. Single Element in a Sorted Array.java
+++ leetcode/BS/540. Single Element in a Sorted Array.java
@@ -1,24 +1,19 @@
 class Solution {
+  
     public int singleNonDuplicate(int[] nums) {
-        int st = 0, end = nums.length - 1, len = nums.length;
-        while(st <= end){
-            int mid = (st + end) / 2;
-            if(mid + 1 < len && nums[mid + 1] == nums[mid]){
-                if(mid % 2 == 0){
-                    st = mid + 2 ;
-                }else{
-                    end = mid - 1;
-                }
-            }else if(mid - 1 >= 0 && nums[mid] == nums[mid - 1]){
-                if(mid % 2 == 0){
-                    end = mid - 2;
-                }else{
-                    st = mid + 1;
-                }
-            }else{
-                return nums[mid];
+        int st = 0;
+        int end = nums.length - 1;
+        while(st < end){
+            int mid = st + (end - st) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                st = mid + 2;
+            } else {
+                end = mid;
             }
         }
-        return -1;
-    }
+        return nums[st];
+    }   
 }

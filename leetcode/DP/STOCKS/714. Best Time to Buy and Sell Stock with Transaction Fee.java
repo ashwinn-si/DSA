42e8c1d073bad765dff6f4160f619f70f54081b9
ashwinn-si <ashwin23cse@gmail.com>
2025-11-08 23:09:15 +0530

    stocks rework made

---
+++ leetcode/DP/STOCKS/714. Best Time to Buy and Sell Stock with Transaction Fee.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int maxProfit(int[] nums, int fee) {
+    int N = nums.length;
+    int dp[][] = new int[N][2]; // 2 -> there can be two states 0 -> no stock | 1 -> has a stock
+    // base case: the first stock should always been bought
+    dp[0][0] = -nums[0];
+
+    for (int index = 1; index < N; index++) {
+      for (int state = 0; state < 2; state++) {
+        int noPick = dp[index - 1][state];
+        int pick = Integer.MIN_VALUE;
+        if (state == 0) {
+          pick = -nums[index] + dp[index - 1][1];
+        } else {
+          pick = nums[index] + dp[index - 1][0] - fee;
+        }
+        dp[index][state] = Math.max(noPick, pick);
+      }
+    }
+    return Math.max(dp[N - 1][0], dp[N - 1][1]);
+  }
+}
